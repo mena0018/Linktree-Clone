4071f6a9e7328640f6551b418da0313c689d6a16
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-02-09 10:36:54 +0100

    Adding a set of favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,10 +4,18 @@ export default function Head() {
       <title>Rabie Menad • Links</title>
       <meta
         name='description'
-        content='Share your Linktree from your Instagram, Github, Twitter and other bios...'
+        content='Partagez le lien de vos réseaux favoris Instagram, Github, Twitter et autres avec Links, une application de partage de liens réalisé avec Next js 13 et Tailwind CSS.'
       />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <link rel='icon' href='/favicon.ico' />
+
+      <link rel='apple-touch-icon' sizes='76x76' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
+
       <meta name='google-site-verification' content='YIuXWESaiIxevrq2WDlHlq6m3uKOzsfMS-Nn_sq_YJA' />
     </>
   );
