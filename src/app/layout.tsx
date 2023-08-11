027efb7b7e2d3d4fd607eaa230e2fd9d22e54b83
mena0018 <rabie.menad07@gmail.com>
2023-08-11 09:17:05 +0200

    🚀 Improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,24 @@ import { Metadata } from 'next';
 import { FC, PropsWithChildren } from 'react';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://rabiemenad.fr'),
+  robots: { index: true, follow: true },
+  themeColor: '#ffffff',
+  manifest: '/site.webmanifest',
   title: 'Rabie Menad • Links',
+  applicationName: 'Rabie Menad',
+  alternates: { canonical: 'https://rabiemenad.fr' },
+  authors: [{ name: 'Rabie Menad', url: 'https://rabiemenad.fr' }],
+  keywords: ['Rabie Menad', 'portfolio', 'développeur', 'react', 'front'],
   description:
-    "Liens de mes réseaux favoris tels que Twitter ou Discord ainsi que d'autres information relatives à mon profil.",
+    "Hello, moi c'est Rabie Menad et ici, je partagerai les liens de mes réseaux favoris ainsi que d'autres informations relatives à mon profil.",
   icons: [
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/png',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
     {
       rel: 'icon',
       type: 'image/png',
@@ -27,10 +41,18 @@ export const metadata: Metadata = {
       url: '/safari-pinned-tab.svg',
     },
   ],
-  themeColor: '#ffffff',
-  manifest: '/site.webmanifest',
-  alternates: {
-    canonical: 'https://rabiemenad.fr',
+  openGraph: {
+    type: 'website',
+    title: 'Rabie Menad • Links',
+    siteName: 'Rabie Menad • Links',
+    url: 'https://rabiemenad.fr',
+    images: [{ url: 'https://ibb.co/pn2zRQJ' }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@veille_teck',
+    creator: '@veille_teck',
+    images: 'https://ibb.co/pn2zRQJ',
   },
 };
 
