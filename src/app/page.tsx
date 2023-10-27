1a92bef9a58b24098344d2fc1ed95a62d3fc2650
Rabie Menad <rabie.menad@ideine.fr>
2023-10-27 10:00:55 +0200

    ✨ Fix some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react';
+import { NextPage } from 'next';
 import Links from '@/components/Links';
 import Header from '@/components/Header';
 import Socials from '@/components/Socials';
 
-const Home: FC = () => {
+const Home: NextPage = () => {
   return (
     <main className='h-full flex flex-col items-center pt-16 px-8'>
       <Header />
